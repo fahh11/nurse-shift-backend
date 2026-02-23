daf3a4a7d3bc0e4a27b744194f6cf25b0a1169fa
fahh11 <phaptawan2546@gmail.com>
2026-02-23 23:15:02 +0700

    feat: add get user hospital api

--- src/interfaces/http/controllers/hospital.controller.ts
+++ src/interfaces/http/controllers/hospital.controller.ts
@@ -3,10 +3,12 @@ import { CreateHospitalBody } from '@service/types/hospital.type'
 import { UpdateHospitalBody } from '@service/types/hospital.type'
 import { createHospital } from '@service/use-cases/hospital/createHospital'
 import { updateHospital } from '@service/use-cases/hospital/updateHospital'
-import { getAllHospital } from '@service/use-cases/hospital/getHospital'
+import { getAllHospital, getUserHospital } from '@service/use-cases/hospital/getHospital'
 import { PrismaHospitalRepository } from '@service/infrastructure/persistence/prisma/repositories/hospital.repository.impl'
+import { PrismaUserRepository } from '@service/infrastructure/persistence/prisma/repositories/user.repository.impl'
 
 const hospitalRepo = new PrismaHospitalRepository()
+const userRepo = new PrismaUserRepository()
 
 export const HospitalController = {
     create: async (request: FastifyRequest<{Body: CreateHospitalBody}>, reply: FastifyReply) => {
@@ -25,6 +27,18 @@ export const HospitalController = {
 
     getAllHospital: async (request: FastifyRequest, reply: FastifyReply) => {
         const result = await getAllHospital(request.log, {hospitalRepo});
+        return reply.send(result)
+    },
+
+    getUserHospital: async (request: FastifyRequest, reply: FastifyReply) => {
+        const currentUser = request.user
+
+        const result = await getUserHospital(
+            currentUser.userId,
+            request.log, 
+            {hospitalRepo, userRepo}
+        );
+
         return reply.send(result)
     },
 }
--- src/use-cases/hospital/getHospital.ts
+++ src/use-cases/hospital/getHospital.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { throwCustomError, ErrorDescription } from '@service/helpers/error'
 import { StatusCode } from '@service/enums/statusCode'
 import { HospitalRepository } from '@service/domain/repositories/hospital.repository'
+import { UserRepository } from '@service/domain/repositories/user.repository'
 
 
 export const getAllHospital = async (
@@ -21,6 +22,39 @@ export const getAllHospital = async (
             };
         })
     );
+
+    return result
+}
+
+export const getUserHospital = async (
+    userId: string,
+    logger: FastifyInstance['log'],
+    repos: {
+        hospitalRepo: HospitalRepository
+        userRepo: UserRepository
+    }
+) => {
+    // หา user ปัจจุบันจาก id
+    const currentUser = await repos.userRepo.findById(userId)
+    if (!currentUser) {
+        logger.error('User not found')
+        throw throwCustomError(ErrorDescription.USER_NOT_FOUND, StatusCode.NOT_FOUND_404)
+    }
+
+    // หา hospital ที่ user อยู่
+    const hospitalData = await repos.hospitalRepo.findById(currentUser.hospitalId!)
+    if (!hospitalData) {
+        logger.error('Hospital not found')
+        throw throwCustomError(
+            ErrorDescription.HOSPITAL_NOT_FOUND,
+            StatusCode.NOT_FOUND_404
+        )
+    }
+
+    const result = {
+        hospitalId: hospitalData.hospitalId,
+        name: hospitalData.name
+    }
 
     return result
 }
